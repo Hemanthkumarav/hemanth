0227e1f75e363d848436db6d0da8fdc264193cd9
User <User@User-PC>
2019-11-20 16:36:32 +0530

    Hetro

--- src/com/str/HetroAll.java
+++ src/com/str/HetroAll.java
@@ -10,6 +10,21 @@ class HetroAll {
 		hetro.add(new Emp1(1,"hhh"));
 		hetro.add(10);
 		System.out.println(hetro);
+		for(int i=0;i<hetro.size();i++) {
+			Object x=hetro.get(i);
+			if(x instanceof Movie1) {
+				System.out.println(((Movie1)x).name);
+				
+				
+			}
+			if(x instanceof Integer) {
+				System.out.println(((Integer)x)+10);
+				
+				
+			}
+
+			
+		}
 	}
 
 }
